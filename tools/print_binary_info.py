05e80e35e2ecd75a845e214efa6084069e3201ea
xxiang <allenxiangxin@gmail.com>
2022-04-20 17:36:33 -0400

    add print recordLen

--- tools/print_binary_info.py
+++ tools/print_binary_info.py
@@ -12,15 +12,18 @@ from caen_reader import RawDataFile
 
 if_path=str(sys.argv[1])
 raw_data_file = RawDataFile(if_path)
-width=10
-print('%s\t%s\t%s\t%s\t%s' % ('i'.ljust(width), "event_id".ljust(width), "boardId".ljust(width), 'TTT'.ljust(width), 'delta_ttt'.ljust(width)))
+width=9
 prev_ttt=0
 for i in range(10000):
     trigger = raw_data_file.getNextTrigger()
     if trigger is None:
         print("Done")
         break
+    if i==0:
+        print("recordLen:", raw_data_file.recordLen) # read one trigger to know recordLen
+        print('%s\t%s\t%s\t%s\t%s' % ('i'.ljust(width), "event_id".ljust(width), "boardId".ljust(width), 'TTT'.ljust(width), 'delta_ttt'.ljust(width) ))
     delta_ttt = trigger.triggerTimeTag - prev_ttt
     print('%s\t%s\t%s\t%s\t%s' % (i, trigger.eventCounter, trigger.boardId, trigger.triggerTimeTag, delta_ttt))
-    #print(i, '\t', trigger.eventCounter,'\t', trigger.boardId, '\t', trigger.triggerTimeTag, '\t', delta_ttt)
+    # for ch, val in trigger.traces.items():
+    #    print(ch, len(val) )
     prev_ttt = trigger.triggerTimeTag
