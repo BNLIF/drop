81f08001fc66044f4ddff46e3ad0df518a6ea391
allenxiangxin <allenxiangxin@gmail.com>
2022-04-27 02:10:48 -0400

    add more enviromental variables

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 # enter environment
 #source env/bin/activate
 
-export PYTHONPATH="${PWD}/src"
+# get the directory of this setup.sh script
+DROP_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+export SOURCE_DIR="${DROP_DIR}/src"
+export YAML_DIR="${DROP_DIR}/yaml"
