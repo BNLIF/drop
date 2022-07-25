f74724c13a2f2d7c90fdf22fcca2d9702da76ea6
xxiang <allenxiangxin@gmail.com>
2022-07-25 14:16:40 -0400

    sanity check; 4 extra bytes are saved by LabVIEW when using three boards, and the boardID are not read-in correctly. Add the special sanity check to caen_reader.py. Still do not know the origin of the 4 bytes but this is a work around

--- src/raw_data_rooter.py
+++ src/raw_data_rooter.py
@@ -30,10 +30,10 @@ from caen_reader import RawDataFile
 #-----------------------------------
 # Global Parameters are Captialized
 #-----------------------------------
-N_BOARDS = 2
+N_BOARDS = 3
 MAX_N_TRIGGERS = 999999 # Arbitary large. Larger than n_triggers in raw binary file.
-DUMP_SIZE = 1000 # number of triggers to accumulate in queue before dump
-INITIAL_BASEKTEL_CAPACITY=1000 # number of basket per file
+DUMP_SIZE = 600 # number of triggers to accumulate in queue before dump
+INITIAL_BASEKTEL_CAPACITY=5000 # number of basket per file
 MAX_EVENT_QUEUE = 10000 # throw warning if event queue is getting too big. No action yet.
 
 if DUMP_SIZE<=10:
@@ -101,7 +101,7 @@ class RawDataRooter():
         self.ch_names = set()
         self.boardId = set()
         raw_data_file = RawDataFile(self.args.if_path)
-        for i in range(20):
+        for i in range(100):
             trg = raw_data_file.getNextTrigger()
             if trg is None:
                 print("Info: current active channels are:")
