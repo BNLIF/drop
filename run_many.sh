e65663d6d9df3a1e81963bac767af414cb951d3c
xxiang <allenxiangxin@gmail.com>
2022-08-03 10:42:45 -0400

    skip line if it starts with #

--- run_many.sh
+++ run_many.sh
@@ -8,6 +8,7 @@ if [ $do_raw_data_rooter -eq 1 ]; then
     output_dir=/media/disk_a/CERNBOX/WbLS-DATA/raw_root/phase0
 
     while read fpath; do
+	case "$fpath" in \#*) continue ;; esac
 	echo " "
 	echo "processing $fpath ..."
 	python src/raw_data_rooter.py --if_path=${fpath} --output_dir=${output_dir}
