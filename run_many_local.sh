f729f4ce7a5c8db6a6526ad8e0be882803f633b6
xxiang <allenxiangxin@gmail.com>
2022-09-29 16:27:12 -0400

    update 1) dqom add sum channel npe trend 2) run_many_local can run all three script at once

--- run_many_local.sh
+++ run_many_local.sh
@@ -9,16 +9,14 @@
 
 #======================================
 # Run Parameters
-do_raw_data_rooter=0
-do_run_drop=0
-do_dq_offline_monitor=1
+run_script_option=0 #0: run all; 1: run raw_data_rooter.py; 2: run_drop.py; 3: dqom.py
 drop_version=v1.0.1
 subdir=muon
 config_file=config.yaml
 #====================================== 
 
-if [ $do_raw_data_rooter -eq 1 ]; then
-    file_path=$1
+function do_raw_data_rooter() {
+    local file_path=$1
     output_dir=/media/disk_a/CERNBOX/WbLS-DATA/raw_root/${subdir}/
     while read fpath; do
 	case "$fpath" in \#*) continue ;; esac
@@ -26,11 +24,10 @@ if [ $do_raw_data_rooter -eq 1 ]; then
 	echo "processing $fpath ..."
 	python src/raw_data_rooter.py --if_path=${fpath} --output_dir=${output_dir}
     done < $file_path
-fi
+}
 
-
-if [ $do_run_drop -eq 1 ]; then
-    file_path=$1
+function do_run_drop() {
+    local file_path=$1
     output_dir=/media/disk_a/CERNBOX/WbLS-DATA/rq/${drop_version}/${subdir}
     mkdir -p $output_dir
     while read fpath; do
@@ -39,10 +36,10 @@ if [ $do_run_drop -eq 1 ]; then
 	echo "processing $fpath ..."
 	python src/run_drop.py -i ${fpath} -c yaml/${config_file} --output_dir=${output_dir}
     done < $file_path
-fi
+}
 
-
-if [ $do_dq_offline_monitor -eq 1 ]; then
+function do_dqom() {
+    local file_path=$1
     output_dir=/media/disk_a/CERNBOX/WbLS-DATA/dqom/${drop_version}/${subdir}
     mkdir -p $output_dir
     file_path=$1
@@ -52,4 +49,35 @@ if [ $do_dq_offline_monitor -eq 1 ]; then
 	echo "processing $fpath ..."
 	python tools/dqom.py ${fpath} ${output_dir}
     done < $file_path
+}
+
+file_path=$1
+if [ $run_script_option -eq 1 ]; then
+    do_raw_data_rooter $file_path
+fi
+
+if [ $run_script_option -eq 2 ]; then
+    do_run_drop $file_path
+fi
+
+if [ $run_script_option -eq 3 ]; then
+    do_dqom $file_path
+fi
+
+if [ $run_script_option -eq 0 ]; then
+    tmp=$(pwd)/tmp.txt
+    cp $file_path $tmp
+    do_raw_data_rooter $tmp
+    
+    sed -i -e 's/binary/root/g' $tmp
+    sed -i -e 's/.bin/.root/g' $tmp
+    do_run_drop $tmp
+
+    olddir="raw_root/${subdir}"
+    newdir="rq/${drop_version}/${subdir}"
+    sed -i "s|$olddir|$newdir|g" $tmp
+    sed -i -e 's/.root/_rq.root/g' $tmp
+    do_dqom $tmp
+
+    rm $tmp
 fi
--- tools/dqom.py
+++ tools/dqom.py
@@ -86,10 +86,50 @@ def plot_channel_noise(file_path, output_dir):
         os.makedirs(directory)
     plt.savefig('%s/%s.pdf' % (directory, start_dt.strftime("%y%m%dT%H%M")))
 
+def plot_npe(file_path, output_dir):
+    start_dt = extract_datetime_from_str(file_path)
+    prefix=start_dt.strftime("%y%m%dT%H%M")
+    with uproot.open(file_path) as f:
+        var_names=[]
+        for k in f['event'].keys():
+            if k[:3]=='ch_':
+                continue
+            var_names.append(k)
+        rq = f['event'].arrays(var_names, library='np')
+
+        branchs= ['pulse_area_sum_pe', 'pulse_area_bot_pe', 'pulse_area_side_pe',
+                 'pulse_area_row1_pe', 'pulse_area_row2_pe', 'pulse_area_row3_pe','pulse_area_row4_pe', 
+                 'pulse_area_col1_pe', 'pulse_area_col2_pe', 'pulse_area_col3_pe','pulse_area_col4_pe']
+        
+        ncol=3
+        nrow=len(branchs)//ncol+1
+        fig, ax = plt.subplots(nrow, ncol, figsize=[4*ncol,3*nrow])
+        ax = ax.flatten()
+        t_minute=rq['event_ttt']*8e-9/60
+        tmax = np.max(t_minute)
+        tmin = np.min(t_minute)
+        nbinx = int((tmax-tmin)/5+0.5) # every 5 minutes a bin                                                                                                                                                               
+        for r, b in enumerate(branchs):
+            a = []
+            t = []
+            for i in range(len(rq[b])):
+                if rq['npulse'][i]>0:
+                    t.append(t_minute[i])
+                    a.append(rq[b][i][0])
+            h0 = ax[r].hist2d(t, a, bins=[nbinx, 100], range=((tmin,tmax),(0, 1000)), norm=colors.LogNorm(), cmap='jet');
+            ax[r].set_xlabel('Time elapsed since run start [min]', fontsize=12)
+            ax[r].set_ylabel('PE', fontsize=12)
+            ax[r].set_title("%s" % b)
+        plt.tight_layout()
+        directory="%s/NpeTrend" % output_dir
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        plt.savefig('%s/%s.pdf' % (directory, start_dt.strftime("%y%m%dT%H%M")))
         
 if __name__ == "__main__":
     if len(sys.argv)==3:
         plot_channel_npe(str(sys.argv[1]), str(sys.argv[2]))
         plot_channel_noise(str(sys.argv[1]), str(sys.argv[2]))
+        plot_npe(str(sys.argv[1]), str(sys.argv[2]))
     else:
         sys.exit("Wrong number of input argument")
