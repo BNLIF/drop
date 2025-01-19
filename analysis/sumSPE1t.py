193ba7d1d707fe9049d0674979b1357d0fd05f48
dilanarik <113949454+dilanarik@users.noreply.github.com>
2025-01-19 11:55:13 -0800

    /

--- analysis/sumSPE1t.py
+++ analysis/sumSPE1t.py
@@ -5,7 +5,7 @@ import sys
 from datetime import datetime
 import os
 
-directory='/media/disk_i/WbLS-DATA/raw_root/phase7/muon'
+directory='/media/disk_i/WbLS-DATA/raw_root/phase7/muon/'
 date=sys.argv[1]
 dateString=datetime.strptime(date,'%y%m%d').strftime('%d %b %Y')
 allRootFiles=os.listdir(directory)
